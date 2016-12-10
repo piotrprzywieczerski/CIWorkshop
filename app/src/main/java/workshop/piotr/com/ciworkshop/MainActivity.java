5f7a3a5aafa2670a345a0be04c3b31b8bc06c246
Piotr Przywieczerski <pprzywieczerski@speednet.pl>
2016-12-10 10:48:18 +0100

    Crashlytics setup.

--- app/src/main/java/workshop/piotr/com/ciworkshop/MainActivity.java
+++ app/src/main/java/workshop/piotr/com/ciworkshop/MainActivity.java
@@ -2,6 +2,8 @@ package workshop.piotr.com.ciworkshop;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -10,6 +12,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
     }
 }
